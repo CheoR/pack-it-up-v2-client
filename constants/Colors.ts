e8d955bd17abc741192e1eeeaceba24fb0285142
CheoR <cheo.roman+github@gmail.com>
2022-12-06 17:19:06 -0600

    build: theme defaults

---
+++ constants/Colors.ts
@@ -0,0 +1,23 @@
+const tintColorLight = '#00171F';
+const tintColorDark = '#FFE8E0';
+
+export default {
+  light: {
+    text: '#00171F',
+    background: '#F2F3EF',
+    tint: tintColorLight,
+    tabIconDefault: '#797B84',
+    tabIconSelected: tintColorLight,
+    warning: '#F40000',
+    action: 'FF6700',
+  },
+  dark: {
+    text: '#FFE8E0',
+    background: '#0D0C10',
+    tint: tintColorDark,
+    tabIconDefault: '#0D0C10',
+    tabIconSelected: tintColorDark,
+    warning: '#FF6700',
+    action: '#0098FF',
+  },
+};
