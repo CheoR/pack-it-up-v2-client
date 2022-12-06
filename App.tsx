82a2fae41901e513588ec9b252bb4a6a15fdeef7
CheoR <cheo.roman+github@gmail.com>
2022-12-06 10:01:42 -0600

    build: passing options example

--- App.tsx
+++ App.tsx
@@ -8,8 +8,10 @@ const Stack = createNativeStackNavigator();
 
 const ROUTES = {
   Landing: "Landing",
+  Login: "Login",
   Register: "Register",
   CreatePost: "CreatePost",
+  DisplayRoute: "DisplayRoute",
 };
 
 function LandingScreen({ navigation, route }) {
@@ -17,8 +19,12 @@ function LandingScreen({ navigation, route }) {
     <View style={styles.landing}>
       <Text>PackItUp</Text>
       <Button
-        title="Register"
+        title={ROUTES.Register}
         onPress={() => navigation.navigate(ROUTES.Register)}
+      />
+      <Button
+        title={ROUTES.Login}
+        onPress={() => navigation.navigate(ROUTES.Login)}
       />
       <Button
         title="Go to Details"
@@ -33,16 +39,31 @@ function LandingScreen({ navigation, route }) {
 
       <Button
         title="Create post"
-        onPress={() => navigation.navigate("CreatePost")}
+        onPress={() => navigation.navigate(ROUTES.CreatePost)}
       />
       <Text style={{ margin: 10 }}>Post: {route.params?.post}</Text>
+      <Button
+        title="I am display route"
+        onPress={() => {
+          navigation.navigate(ROUTES.DisplayRoute, {
+            itemId: 86,
+            someVariable: "i am variable",
+          });
+        }}
+      />
       <StatusBar style="auto" />
     </View>
   );
 }
 
 function RegisterScreen({ route, navigation }) {
-  const { itemId, someVariable } = route.params;
+  let itemId,
+    someVariable = "";
+  try {
+    ({ itemId, someVariable } = route.params);
+  } catch (error) {
+    console.log("");
+  }
 
   return (
     <View style={styles.register}>
@@ -61,7 +82,29 @@ function RegisterScreen({ route, navigation }) {
       />
       <Button
         title="Go to Home"
-        onPress={() => navigation.navigate("Landing")}
+        onPress={() => navigation.navigate(ROUTES.Landing)}
+      />
+      <Button title="Go back" onPress={() => navigation.goBack()} />
+      <Button
+        title="Go back to first screen in stack"
+        onPress={() => navigation.popToTop()}
+      />
+    </View>
+  );
+}
+
+function LoginScreen({ navigation }) {
+  return (
+    <View style={styles.login}>
+      <Text>Login Screen</Text>
+
+      <Button
+        title={ROUTES.Register}
+        onPress={() => navigation.push(ROUTES.Register)}
+      />
+      <Button
+        title="Go to Home"
+        onPress={() => navigation.navigate(ROUTES.Landing)}
       />
       <Button title="Go back" onPress={() => navigation.goBack()} />
       <Button
@@ -89,13 +132,49 @@ function CreatePostScreen({ navigation, route }) {
         onPress={() => {
           // Pass and merge params back to home screen
           navigation.navigate({
-            name: "Landing",
+            name: ROUTES.Landing,
             params: { post: postText },
             merge: true,
           });
         }}
       />
     </>
+  );
+}
+
+function DisplayRouteScreen({ navigation, route }) {
+  const [count, setCount] = React.useState(0);
+
+  React.useEffect(() => {
+    // Use `setOptions` to update the button that we previously specified
+    // Now the button includes an `onPress` handler to update the count
+    navigation.setOptions({
+      headerRight: () => (
+        <Button onPress={() => setCount((c) => c + 1)} title="CountUpdated" />
+      ),
+    });
+  }, [navigation]);
+
+  return (
+    <View style={styles.register}>
+      <Text>Display route Screen</Text>
+      <Text>{route.params.name}</Text>
+      <Text>Count: {count}</Text>
+
+      <Button
+        title="Go to Details... again"
+        onPress={() => navigation.push(ROUTES.Register)}
+      />
+      <Button
+        title="Go to Home"
+        onPress={() => navigation.navigate(ROUTES.Landing)}
+      />
+      <Button title="Go back" onPress={() => navigation.goBack()} />
+      <Button
+        title="Go back to first screen in stack"
+        onPress={() => navigation.popToTop()}
+      />
+    </View>
   );
 }
 
@@ -117,8 +196,23 @@ export default function App() {
           component={LandingScreen}
           options={{ title: "PackItUp" }}
         />
+        <Stack.Screen name={ROUTES.Login} component={LoginScreen} />
         <Stack.Screen name={ROUTES.Register} component={RegisterScreen} />
         <Stack.Screen name={ROUTES.CreatePost} component={CreatePostScreen} />
+        <Stack.Screen
+          name={ROUTES.DisplayRoute}
+          component={DisplayRouteScreen}
+          options={({ route }) => ({
+            title: route.params.name || "route.params.name not found",
+            headerRight: () => (
+              <Button
+                onPress={() => alert("This is a button!")}
+                title="Info"
+                color="#fff"
+              />
+            ),
+          })}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
@@ -130,6 +224,12 @@ const styles = StyleSheet.create({
     backgroundColor: "red",
     alignItems: "center",
     justifyContent: "center",
+  },
+  login: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "pink",
   },
   register: {
     flex: 1,
