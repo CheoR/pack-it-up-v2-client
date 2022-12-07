f3c09cd88f19814906106002c2795b826ebccf7d
CheoR <cheo.roman+github@gmail.com>
2022-12-07 09:45:51 -0600

    build: Login screen

--- screens/Login.tsx
+++ screens/Login.tsx
@@ -1,33 +1,62 @@
-import { StyleSheet, Button, Text, View } from "react-native";
+import { StyleSheet, Button, Text, View, TextInput } from "react-native";
+
+import SocialsIcons from "../components/SocialsIcons";
+import BoxLogo from "../components/BoxLogo";
+import Colors from "../constants/Colors";
 import ROUTES from "../constants/Routes";
 
 export default function LoginScreen({ navigation }) {
   return (
     <View style={styles.login}>
-      <Text>Login Screen</Text>
+      <BoxLogo />
 
-      <Button
-        title={ROUTES.Register}
-        onPress={() => navigation.push(ROUTES.Register)}
-      />
-      <Button
-        title="Go to Home"
-        onPress={() => navigation.navigate(ROUTES.Landing)}
-      />
-      <Button title="Go back" onPress={() => navigation.goBack()} />
-      <Button
-        title="Go back to first screen in stack"
-        onPress={() => navigation.popToTop()}
-      />
+      <View style={styles.container}>
+        <View style={styles.inputGroup}>
+          <TextInput placeholder="Username" />
+          <TextInput placeholder="Password" secureTextEntry={true} />
+        </View>
+      </View>
+      <View style={{ width: "100%" }}>
+        <Button
+          title={ROUTES.Login}
+          color={Colors.light.tint}
+          onPress={() => navigation.navigate(ROUTES.Register)}
+        />
+        <View style={{ alignItems: "center" }}>
+          <Text>
+            No Account,{" "}
+            <Text
+              style={{ color: Colors.light.action }}
+              onPress={() => navigation.push(ROUTES.Register)}
+            >
+              Register!
+            </Text>
+          </Text>
+        </View>
+      </View>
+
+      <SocialsIcons />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  login: {
-    flex: 1,
+  container: {
+    width: "80%",
+    height: 60,
     alignItems: "center",
-    justifyContent: "center",
-    backgroundColor: "pink",
+  },
+  inputGroup: {
+    flex: 1,
+    justifyContent: "space-between",
+    width: "100%",
+  },
+  login: {
+    alignItems: "center",
+    justifyContent: "space-between",
+    height: "100%",
+    paddingVertical: 24,
+    paddingHorizontal: 16,
+    backgroundColor: Colors.light.background,
   },
 });
