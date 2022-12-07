3ef71369c18060e9d0481e8313fb179c4d0fcc95
CheoR <cheo.roman+github@gmail.com>
2022-12-07 08:46:21 -0600

    styles: move into create styles

--- screens/Landing.tsx
+++ screens/Landing.tsx
@@ -1,58 +1,67 @@
 import { StyleSheet, Button, Text, View, ScrollView } from "react-native";
-import { StatusBar } from "expo-status-bar";
+
+import SocialsIcons from "../components/SocialsIcons";
+import SvgComponent from "../components/Rafiki";
+import BoxLogo from "../components/BoxLogo";
 import ROUTES from "../constants/Routes";
 import Colors from "../constants/Colors";
 
-import SvgComponent from "../components/Rafiki";
-import SocialsIcons from "../components/SocialsIcons";
-import BoxLogo from "../components/BoxLogo";
-
 export default function LandingScreen({ navigation, route }) {
   return (
-    <ScrollView
-      contentContainerStyle={{
-        alignItems: "center",
-        justifyContent: "center",
-        paddingVertical: 24,
-        paddingHorizontal: 16,
-        backgroundColor: Colors.light.background,
-      }}
-    >
+    <ScrollView contentContainerStyle={styles.contentContainerStyle}>
       <BoxLogo />
       <Text style={styles.header}>track what you pack, app</Text>
-      <Button
-        title={ROUTES.Register}
-        onPress={() => navigation.navigate(ROUTES.Register)}
-      />
-      <Button
-        title={ROUTES.Login}
-        onPress={() => navigation.navigate(ROUTES.Login)}
-      />
+      <View style={styles.buttonGroup}>
+        <Button
+          title={ROUTES.Register}
+          color={Colors.light.tabIconDefault}
+          onPress={() => navigation.navigate(ROUTES.Register)}
+        />
+        <Button
+          title={ROUTES.Login}
+          color={Colors.light.tint}
+          onPress={() => navigation.navigate(ROUTES.Login)}
+        />
+      </View>
       <SvgComponent />
-      <View
-        style={{
-          width: "80%",
-        }}
-      >
+      <View style={styles.tagline}>
         <Text>Organize your life.</Text>
       </View>
       <SocialsIcons />
-
-      <StatusBar style="auto" />
     </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
+  contentContainerStyle: {
+    alignItems: "center",
+    justifyContent: "center",
+    paddingVertical: 24,
+    paddingHorizontal: 16,
+    backgroundColor: Colors.light.background,
+  },
+  buttonGroup: {
+    flex: 1,
+    width: "100%",
+    margin: 24,
+    justifyContent: "space-between",
+    height: 80,
+  },
+
+  header: {
+    fontSize: 64,
+    color: Colors.light.tint,
+  },
+  tagline: {
+    width: "80%",
+    display: "flex",
+    alignItems: "center",
+  },
   landing: {
     flex: 1,
     backgroundColor: Colors.light.background,
     alignItems: "center",
     justifyContent: "center",
     paddingTop: 0,
-  },
-  header: {
-    fontSize: 64,
-    color: Colors.light.warning,
   },
 });
