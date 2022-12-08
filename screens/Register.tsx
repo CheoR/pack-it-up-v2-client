e784225f40e60ba96c193b23d50bed6b8a3ed99b
CheoR <cheo.roman+github@gmail.com>
2022-12-08 15:42:33 -0600

    refactor(Register): to use Layout

--- screens/Register.tsx
+++ screens/Register.tsx
@@ -1,73 +1,75 @@
-import { StyleSheet, Button, Text, TextInput, View } from "react-native";
+import { Button, StyleSheet, Text, TextInput, View } from "react-native";
 
-import SocialsIcons from "../components/SocialsIcons";
-import BoxLogo from "../components/BoxLogo";
 import Colors from "../constants/Colors";
 import ROUTES from "../constants/Routes";
+import Layout from "../layout/Layout";
 
-export default function RegisterScreen({ route, navigation }) {
-  let itemId,
-    someVariable = "";
-  try {
-    ({ itemId, someVariable } = route.params);
-  } catch (error) {
-    console.log("");
-  }
-
+export default function RegisterScreen({ navigation }) {
   return (
-    <View style={styles.register}>
-      <BoxLogo />
-      <View style={styles.container}>
-        <View style={styles.inputGroup}>
-          <TextInput placeholder="Username" />
-          <TextInput placeholder="First Name" />
-          <TextInput placeholder="Last Name" />
-          <TextInput placeholder="Email" />
-          <TextInput placeholder="Password" secureTextEntry={true} />
-          <TextInput placeholder="Confirm Password" secureTextEntry={true} />
+    <Layout>
+      <View style={styles.screen}>
+        <View style={styles.inputBlock}>
+          <TextInput placeholder="Username" style={styles.input} />
+          <TextInput placeholder="First Name" style={styles.input} />
+          <TextInput placeholder="Last Name" style={styles.input} />
+          <TextInput placeholder="Email" style={styles.input} />
+          <TextInput
+            placeholder="Password"
+            style={styles.input}
+            secureTextEntry={true}
+          />
+          <TextInput
+            placeholder="Confirm Password"
+            style={styles.input}
+            secureTextEntry={true}
+          />
         </View>
-      </View>
-
-      <View style={{ width: "100%" }}>
-        <Button
-          title={ROUTES.Register}
-          color={Colors.light.tabIconDefault}
-          onPress={() => navigation.navigate(ROUTES.Landing)}
-        />
-        <View style={{ alignItems: "center" }}>
-          <Text>
-            Already Registered,{" "}
-            <Text
-              style={{ color: Colors.light.action }}
-              onPress={() => navigation.push(ROUTES.Register)}
-            >
-              Login!
+        <View style={styles.action}>
+          <Button
+            title={ROUTES.Register}
+            color={Colors.light.tabIconDefault}
+            onPress={() => navigation.navigate(ROUTES.Landing)}
+          />
+          <View style={styles.actionBlock}>
+            <Text>
+              Already Registered,{" "}
+              <Text
+                style={styles.actionBlockText}
+                onPress={() => navigation.push(ROUTES.Register)}
+              >
+                Login!
+              </Text>
             </Text>
-          </Text>
+          </View>
         </View>
       </View>
-      <SocialsIcons />
-    </View>
+    </Layout>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    width: "80%",
-    height: 160,
-    alignItems: "center",
-  },
-  inputGroup: {
-    flex: 1,
-    justifyContent: "space-between",
+  action: {
     width: "100%",
   },
-  register: {
+  actionBlock: {
     alignItems: "center",
+  },
+  actionBlockText: {
+    color: Colors.light.action,
+  },
+  input: {
+    borderBottomColor: Colors.light.tabIconDefault,
+    borderBottomWidth: 8,
+  },
+  inputBlock: {
     justifyContent: "space-between",
-    height: "100%",
-    paddingVertical: 24,
-    paddingHorizontal: 16,
-    backgroundColor: Colors.light.background,
+    width: "80%",
+    height: 300,
+  },
+  screen: {
+    flex: 1,
+    width: "100%",
+    alignItems: "center",
+    justifyContent: "space-around",
   },
 });
