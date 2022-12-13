d05658bc0e9d7b0c3b76f3e2a9da103187f7c43a
CheoR <cheo.roman+github@gmail.com>
2022-12-13 08:50:03 -0600

    buid: Home screen

--- screens/Home.tsx
+++ screens/Home.tsx
@@ -1,9 +1,16 @@
 import React from "react";
-import { StyleSheet, Text, TextInput, View } from "react-native";
+import { FlatList, StyleSheet, Text, TextInput, View } from "react-native";
 import Checkbox from "expo-checkbox";
-import Constants from "expo-constants";
 
+import LoggedInLayout from "../layout/LoggedInLayout";
+import ListItem from "../components/ListItem";
 import Colors from "../constants/Colors";
+
+const DATA = [
+  { id: "moves", count: 3, type: "move" },
+  { id: "boxes", count: 2, type: "box" },
+  { id: "items", count: 1, type: "item" },
+];
 
 export default function HomeScreen({ route }) {
   const response = {};
@@ -17,70 +24,77 @@ export default function HomeScreen({ route }) {
     Object.assign(response, registerUser);
   }
   const { token, email, username } = response;
-  const [isChecked, setChecked] = React.useState(true);
+  const [isChecked, setIsChecked] = React.useState(true);
 
   return (
-    <View style={styles.screen}>
-      <View style={styles.viewHeader}>
-        <Text style={styles.viewHeaderText}>Summary</Text>
-      </View>
-      <View>
-        <View>
-          <Text>Moves</Text>
+    <LoggedInLayout>
+      <View style={styles.screen}>
+        <View style={styles.viewHeader}>
+          <Text style={styles.viewHeaderText}>Summary</Text>
         </View>
-        <View>
-          <Text>Boxes</Text>
-        </View>
-        <View>
-          <Text>Items</Text>
-        </View>
-      </View>
-      <View style={{ backgroundColor: "pink" }}>
-        <View style={styles.valueInput}>
-          <Text style={{ backgroundColor: "yellow" }}>total</Text>
-          <TextInput style={{ backgroundColor: "green" }} editable={false}>
-            $100
-          </TextInput>
-        </View>
-        <View style={styles.checkboxContainer}>
-          {/* TODO: swap out checkbox and remove from packages */}
-          <Checkbox
-            style={styles.checkbox}
-            value={isChecked}
-            onValueChange={setChecked}
-            disabled
-            color={isChecked ? "#4630EB" : undefined}
+        {/* <View style={styles.listItems}>
+          <ListItem count={1} type="move" />
+          <ListItem count={2} type="box" />
+          <ListItem count={3} type="item" />
+        </View> */}
+        <View style={styles.listItems}>
+          <FlatList
+            data={DATA}
+            keyExtractor={(item) => item.id}
+            renderItem={({ item }) => (
+              <ListItem count={item.count} type={item.type} />
+            )}
           />
-          <Text style={styles.label}>Fragile</Text>
+        </View>
+        <View>
+          <View style={styles.valueInput}>
+            <Text>Total</Text>
+            <TextInput editable={false}>$100</TextInput>
+          </View>
+          <View style={styles.checkboxContainer}>
+            {/* TODO: swap out checkbox and remove from packages */}
+            <Checkbox
+              style={styles.checkbox}
+              value={isChecked}
+              onValueChange={setIsChecked}
+              disabled
+              color={isChecked ? Colors.light.tabIconSelected : undefined}
+            />
+            <Text style={styles.label}>Fragile</Text>
+          </View>
         </View>
       </View>
-    </View>
+    </LoggedInLayout>
   );
 }
 
 const styles = StyleSheet.create({
+  listItems: {
+    height: 550,
+    justifyContent: "space-evenly",
+    width: "100%",
+  },
   screen: {
-    marginTop: Constants.statusBarHeight,
     alignItems: "center",
-    justifyContent: "space-between",
-    paddingVertical: 24,
-    paddingHorizontal: 16,
-    // backgroundColor: Colors.light.background,
-    backgroundColor: "lightblue",
     flex: 1,
+    justifyContent: "space-between",
     width: "100%",
   },
   viewHeader: {
+    alignItems: "center",
     backgroundColor: Colors.light.tabIconDefault,
     display: "flex",
-    alignItems: "center",
-    width: "80%",
+    borderTopLeftRadius: 0,
+    borderTopRightRadius: 0,
+    borderBottomLeftRadius: 6,
+    borderBottomRightRadius: 6,
     paddingVertical: 8,
+    width: "80%",
   },
   valueInput: {
     flexDirection: "row",
     justifyContent: "space-between",
-    backgroundColor: "lightgreen",
+    alignItems: "center",
     width: 300,
   },
   viewHeaderText: {
