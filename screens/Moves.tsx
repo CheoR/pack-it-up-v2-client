0c617ab3fe9137ba783bc43f466c2391daebe873
CheoR <cheo.roman+github@gmail.com>
2022-12-15 14:17:18 -0600

    build: Moves list screen

---
+++ screens/Moves.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+import LoggedInLayout from "../layout/LoggedInLayout";
+
+export default function MovesScreen() {
+  return (
+    <LoggedInLayout>
+      <View>
+        <Text>MovesScreen</Text>
+      </View>
+    </LoggedInLayout>
+  );
+}
