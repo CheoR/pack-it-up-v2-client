89e0142f44bc5dfeb6edf8f66be7b03f4e08c8c2
CheoR <cheo.roman+github@gmail.com>
2022-12-15 14:17:49 -0600

    build: Boxes list screen

---
+++ screens/Boxes.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+import LoggedInLayout from "../layout/LoggedInLayout";
+
+export default function BoxesScreen() {
+  return (
+    <LoggedInLayout>
+      <View>
+        <Text>BoxesScreen</Text>
+      </View>
+    </LoggedInLayout>
+  );
+}
