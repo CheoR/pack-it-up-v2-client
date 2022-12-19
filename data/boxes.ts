3d11a5444b44c030407d2648c67d5d8ffdee7e8f
CheoR <cheo.roman+github@gmail.com>
2022-12-19 08:41:26 -0600

    chore: Boxes screen mock data

---
+++ data/boxes.ts
@@ -0,0 +1,203 @@
+const DATA = [{
+  "id": 1,
+  "move_id": 16,
+  "name": "justo sollicitudin ut",
+  "description": "Curabitur gravida nisi at nibh. In hac habitasse platea dictumst."
+}, {
+  "id": 2,
+  "move_id": 22,
+  "name": "dictumst aliquam augue",
+  "description": "Nullam sit amet turpis elementum ligula vehicula consequat. Morbi a ipsum."
+}, {
+  "id": 3,
+  "move_id": 14,
+  "name": "integer tincidunt ante",
+  "description": "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Proin interdum mauris non ligula pellentesque ultrices."
+}, {
+  "id": 4,
+  "move_id": 6,
+  "name": "sociis natoque",
+  "description": "Pellentesque eget nunc."
+}, {
+  "id": 5,
+  "move_id": 1,
+  "name": "congue",
+  "description": "Donec quis orci eget orci vehicula condimentum. Curabitur in libero ut massa volutpat convallis."
+}, {
+  "id": 6,
+  "move_id": 9,
+  "name": "in",
+  "description": "Donec semper sapien a libero. Nam dui."
+}, {
+  "id": 7,
+  "move_id": 21,
+  "name": "ultrices posuere",
+  "description": "Nunc rhoncus dui vel sem."
+}, {
+  "id": 8,
+  "move_id": 11,
+  "name": "luctus cum",
+  "description": "Donec posuere metus vitae ipsum. Aliquam non mauris."
+}, {
+  "id": 9,
+  "move_id": 23,
+  "name": "magna bibendum",
+  "description": "Integer a nibh. In quis justo."
+}, {
+  "id": 10,
+  "move_id": 13,
+  "name": "consequat",
+  "description": "Phasellus sit amet erat."
+}, {
+  "id": 11,
+  "move_id": 24,
+  "name": "sit amet",
+  "description": "Donec diam neque, vestibulum eget, vulputate ut, ultrices vel, augue."
+}, {
+  "id": 12,
+  "move_id": 25,
+  "name": "parturient montes",
+  "description": "Vivamus vel nulla eget eros elementum pellentesque."
+}, {
+  "id": 13,
+  "move_id": 2,
+  "name": "augue a suscipit",
+  "description": "Sed ante. Vivamus tortor."
+}, {
+  "id": 14,
+  "move_id": 20,
+  "name": "curabitur in libero",
+  "description": "Vestibulum quam sapien, varius ut, blandit non, interdum in, ante."
+}, {
+  "id": 15,
+  "move_id": 3,
+  "name": "cubilia",
+  "description": "Maecenas rhoncus aliquam lacus."
+}, {
+  "id": 16,
+  "move_id": 12,
+  "name": "est et",
+  "description": "Vivamus tortor."
+}, {
+  "id": 17,
+  "move_id": 8,
+  "name": "sed lacus",
+  "description": "Vivamus vestibulum sagittis sapien."
+}, {
+  "id": 18,
+  "move_id": 30,
+  "name": "nulla",
+  "description": "Integer tincidunt ante vel ipsum. Praesent blandit lacinia erat."
+}, {
+  "id": 19,
+  "move_id": 28,
+  "name": "elementum pellentesque quisque",
+  "description": "Morbi odio odio, elementum eu, interdum eu, tincidunt in, leo."
+}, {
+  "id": 20,
+  "move_id": 19,
+  "name": "quis",
+  "description": "Curabitur convallis. Duis consequat dui nec nisi volutpat eleifend."
+}, {
+  "id": 21,
+  "move_id": 17,
+  "name": "urna pretium nisl",
+  "description": "Curabitur at ipsum ac tellus semper interdum."
+}, {
+  "id": 22,
+  "move_id": 18,
+  "name": "magna ac",
+  "description": "Pellentesque viverra pede ac diam."
+}, {
+  "id": 23,
+  "move_id": 10,
+  "name": "ut",
+  "description": "Donec ut mauris eget massa tempor convallis. Nulla neque libero, convallis eget, eleifend luctus, ultricies eu, nibh."
+}, {
+  "id": 24,
+  "move_id": 5,
+  "name": "ullamcorper augue a",
+  "description": "In eleifend quam a odio. In hac habitasse platea dictumst."
+}, {
+  "id": 25,
+  "move_id": 4,
+  "name": "volutpat in",
+  "description": "Quisque erat eros, viverra eget, congue eget, semper rutrum, nulla."
+}, {
+  "id": 26,
+  "move_id": 29,
+  "name": "in consequat ut",
+  "description": "Nulla facilisi. Cras non velit nec nisi vulputate nonummy."
+}, {
+  "id": 27,
+  "move_id": 26,
+  "name": "arcu",
+  "description": "Nam dui. Proin leo odio, porttitor id, consequat in, consequat ut, nulla."
+}, {
+  "id": 28,
+  "move_id": 27,
+  "name": "ultrices",
+  "description": "Suspendisse potenti."
+}, {
+  "id": 29,
+  "move_id": 15,
+  "name": "vel",
+  "description": "Nam tristique tortor eu pede."
+}, {
+  "id": 30,
+  "move_id": 7,
+  "name": "et commodo vulputate",
+  "description": "Integer tincidunt ante vel ipsum."
+}, {
+  "id": 31,
+  "move_id": 18,
+  "name": "at nunc commodo",
+  "description": "In hac habitasse platea dictumst. Etiam faucibus cursus urna."
+}, {
+  "id": 32,
+  "move_id": 16,
+  "name": "sagittis sapien cum",
+  "description": "Vivamus metus arcu, adipiscing molestie, hendrerit at, vulputate vitae, nisl. Aenean lectus."
+}, {
+  "id": 33,
+  "move_id": 24,
+  "name": "vel enim",
+  "description": "Vivamus vel nulla eget eros elementum pellentesque. Quisque porta volutpat erat."
+}, {
+  "id": 34,
+  "move_id": 30,
+  "name": "imperdiet",
+  "description": "In blandit ultrices enim."
+}, {
+  "id": 35,
+  "move_id": 4,
+  "name": "turpis sed",
+  "description": "Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Duis faucibus accumsan odio. Curabitur convallis."
+}, {
+  "id": 36,
+  "move_id": 13,
+  "name": "pellentesque",
+  "description": "Proin risus. Praesent lectus."
+}, {
+  "id": 37,
+  "move_id": 3,
+  "name": "ultrices enim",
+  "description": "Nullam porttitor lacus at turpis."
+}, {
+  "id": 38,
+  "move_id": 8,
+  "name": "nunc rhoncus dui",
+  "description": "Nulla facilisi. Cras non velit nec nisi vulputate nonummy."
+}, {
+  "id": 39,
+  "move_id": 1,
+  "name": "leo odio",
+  "description": "Morbi a ipsum."
+}, {
+  "id": 40,
+  "move_id": 7,
+  "name": "sed interdum",
+  "description": "Morbi sem mauris, laoreet ut, rhoncus aliquet, pulvinar sed, nisl. Nunc rhoncus dui vel sem."
+}]
+
+export default DATA
