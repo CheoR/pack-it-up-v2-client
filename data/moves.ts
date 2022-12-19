8ce1c58c4c623cca392c4bf00cc83634a725f712
CheoR <cheo.roman+github@gmail.com>
2022-12-19 08:43:12 -0600

    build: update data format reflect UI

--- data/moves.ts
+++ data/moves.ts
@@ -1,43 +1,123 @@
-const DATA = [
-  { id: 1, count: 3, type: "move" },
-  { id: 2, count: 2, type: "box" },
-  { id: 3, count: 1, type: "item" },
-  { id: 4, count: 3, type: "move" },
-  { id: 5, count: 2, type: "box" },
-  { id: 6, count: 1, type: "item" },
-  { id: 7, count: 3, type: "move" },
-  { id: 8, count: 2, type: "box" },
-  { id: 9, count: 1, type: "item" },
-  { id: 10, count: 3, type: "move" },
-  { id: 11, count: 2, type: "box" },
-  { id: 12, count: 1, type: "item" },
-  { id: 13, count: 1, type: "item" },
-  { id: 14, count: 3, type: "move" },
-  { id: 15, count: 2, type: "box" },
-  { id: 16, count: 1, type: "item" },
-  { id: 17, count: 3, type: "move" },
-  { id: 18, count: 2, type: "box" },
-  { id: 19, count: 1, type: "item" },
-  { id: 20, count: 3, type: "move" },
-  { id: 21, count: 2, type: "box" },
-  { id: 22, count: 1, type: "item" },
-  { id: 23, count: 1, type: "item" },
-  { id: 24, count: 3, type: "move" },
-  { id: 25, count: 2, type: "box" },
-  { id: 26, count: 1, type: "item" },
-  { id: 27, count: 3, type: "move" },
-  { id: 28, count: 2, type: "box" },
-  { id: 29, count: 1, type: "item" },
-  { id: 30, count: 3, type: "move" },
-  { id: 31, count: 2, type: "box" },
-  { id: 32, count: 1, type: "item" },
-  { id: 33, count: 1, type: "item" },
-  { id: 34, count: 3, type: "move" },
-  { id: 35, count: 2, type: "box" },
-  { id: 36, count: 1, type: "item" },
-  { id: 37, count: 3, type: "move" },
-  { id: 38, count: 2, type: "box" },
-  { id: 39, count: 1, type: "item" },
-];
+const DATA = [{
+  "id": 1,
+  "name": "Tools",
+  "description": "Nulla ut erat id mauris vulputate elementum."
+}, {
+  "id": 2,
+  "name": "Computers",
+  "description": "Curabitur in libero ut massa volutpat convallis. Morbi odio odio, elementum eu, interdum eu, tincidunt in, leo."
+}, {
+  "id": 3,
+  "name": "Toys",
+  "description": "Integer a nibh."
+}, {
+  "id": 4,
+  "name": "Clothing",
+  "description": "Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Duis faucibus accumsan odio. Curabitur convallis."
+}, {
+  "id": 5,
+  "name": "Clothing",
+  "description": "In tempor, turpis nec euismod scelerisque, quam turpis adipiscing lorem, vitae mattis nibh ligula nec sem. Duis aliquam convallis nunc."
+}, {
+  "id": 6,
+  "name": "Home",
+  "description": "Suspendisse accumsan tortor quis turpis. Sed ante."
+}, {
+  "id": 7,
+  "name": "Automotive",
+  "description": "Vestibulum quam sapien, varius ut, blandit non, interdum in, ante."
+}, {
+  "id": 8,
+  "name": "Beauty",
+  "description": "In sagittis dui vel nisl. Duis ac nibh."
+}, {
+  "id": 9,
+  "name": "Toys",
+  "description": "In congue."
+}, {
+  "id": 10,
+  "name": "Clothing",
+  "description": "Aenean lectus."
+}, {
+  "id": 11,
+  "name": "Industrial",
+  "description": "In blandit ultrices enim. Lorem ipsum dolor sit amet, consectetuer adipiscing elit."
+}, {
+  "id": 12,
+  "name": "Electronics",
+  "description": "Praesent lectus. Vestibulum quam sapien, varius ut, blandit non, interdum in, ante."
+}, {
+  "id": 13,
+  "name": "Kids",
+  "description": "Nullam varius. Nulla facilisi."
+}, {
+  "id": 14,
+  "name": "Home",
+  "description": "Nunc nisl. Duis bibendum, felis sed interdum venenatis, turpis enim blandit mi, in porttitor pede justo eu massa."
+}, {
+  "id": 15,
+  "name": "Toys",
+  "description": "Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Etiam vel augue."
+}, {
+  "id": 16,
+  "name": "Industrial",
+  "description": "Mauris lacinia sapien quis libero. Nullam sit amet turpis elementum ligula vehicula consequat."
+}, {
+  "id": 17,
+  "name": "Tools",
+  "description": "Sed accumsan felis."
+}, {
+  "id": 18,
+  "name": "Sports",
+  "description": "Integer aliquet, massa id lobortis convallis, tortor risus dapibus augue, vel accumsan tellus nisi eu orci. Mauris lacinia sapien quis libero."
+}, {
+  "id": 19,
+  "name": "Jewelry",
+  "description": "Vestibulum ac est lacinia nisi venenatis tristique."
+}, {
+  "id": 20,
+  "name": "Toys",
+  "description": "Aliquam erat volutpat."
+}, {
+  "id": 21,
+  "name": "Movies",
+  "description": "Etiam justo. Etiam pretium iaculis justo."
+}, {
+  "id": 22,
+  "name": "Electronics",
+  "description": "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Proin risus."
+}, {
+  "id": 23,
+  "name": "Shoes",
+  "description": "Nullam porttitor lacus at turpis. Donec posuere metus vitae ipsum."
+}, {
+  "id": 24,
+  "name": "Kids",
+  "description": "Donec ut mauris eget massa tempor convallis."
+}, {
+  "id": 25,
+  "name": "Beauty",
+  "description": "Etiam faucibus cursus urna. Ut tellus."
+}, {
+  "id": 26,
+  "name": "Music",
+  "description": "Suspendisse potenti. Nullam porttitor lacus at turpis."
+}, {
+  "id": 27,
+  "name": "Home",
+  "description": "Cras in purus eu magna vulputate luctus."
+}, {
+  "id": 28,
+  "name": "Garden",
+  "description": "Integer ac neque. Duis bibendum."
+}, {
+  "id": 29,
+  "name": "Industrial",
+  "description": "Duis bibendum, felis sed interdum venenatis, turpis enim blandit mi, in porttitor pede justo eu massa. Donec dapibus."
+}, {
+  "id": 30,
+  "name": "Tools",
+  "description": "Morbi non quam nec dui luctus rutrum. Nulla tellus."
+}]
 
 export default DATA;
