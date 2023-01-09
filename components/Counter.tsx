ca31364957bbfd11e49ae3234648448a6f7fb533
CheoR <cheo.roman+github@gmail.com>
2023-01-09 10:51:25 -0600

    build: Counter

--- components/Counter.tsx
+++ components/Counter.tsx
@@ -1,18 +1,43 @@
 import React, { useState } from "react";
-import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
-
+// import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
 import { Button, StyleSheet, Text, View } from "react-native";
+
 import COLORS from "../constants/Colors";
 
 {
   /* <MaterialCommunityIcons name="minus" size={24} color={COLORS.light.tint} />; */
 }
 
-export default function Counter({ type }) {
+import { gql, useMutation } from "@apollo/client";
+
+const CREATE_MOVE = gql`
+  mutation CreateMove($input: CreateMoveInput!) {
+    createMove(input: $input) {
+      _id
+    }
+  }
+`;
+
+const createMove = (count: number) => {
+  const [createMove, { data, loading, error }] = useMutation(CREATE_MOVE, {
+    variables: {
+      input: count,
+    },
+    onCompleted: (data) => {
+      // refresh data
+      console.log(`created ${count} moves`);
+    },
+    onError: (error) => {
+      console.log(error.message);
+    },
+  });
+};
+
+export default function Counter({ action, type }) {
   const [count, setCount] = useState(0);
   return (
     <View style={styles.counter}>
-      <View style={styles.counts}>
+      <View style={styles.incrementDecrement}>
         <Button
           title="&#8331;"
           color={COLORS.light.tabIconDefault}
@@ -35,7 +60,7 @@ export default function Counter({ type }) {
         <Button
           title={`&#9744; Create ${type}`}
           color={COLORS.light.action}
-          onPress={() => console.log(count)}
+          onPress={() => action(count)}
         />
       </View>
     </View>
@@ -44,9 +69,10 @@ export default function Counter({ type }) {
 
 const styles = StyleSheet.create({
   counter: {
+    marginTop: 16,
     width: "100%",
   },
-  counts: {
+  incrementDecrement: {
     flexDirection: "row",
     justifyContent: "space-between",
   },
