634f16c0f2b5d6f44ce934f97a15e120942ce688
CheoR <cheo.roman+github@gmail.com>
2023-01-23 14:11:39 -0600

    build: update caching policy to refetch if data changed

--- App.tsx
+++ App.tsx
@@ -16,6 +16,7 @@ const client = new ApolloClient({
   uri: "http://192.168.1.65:4000/",
   cache: new InMemoryCache(),
   credentials: "include",
+  defaultOptions: { watchQuery: { fetchPolicy: "cache-and-network" } },
 });
 
 export default function App() {
