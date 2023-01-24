f52aac5e38f066d03081e180c4e614f465dcddbe
CheoR <cheo.roman+github@gmail.com>
2023-01-24 09:06:25 -0600

    refactor: cache to own file

--- App.tsx
+++ App.tsx
@@ -1,12 +1,13 @@
 import React from "react";
-import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
+import { ApolloClient, ApolloProvider } from "@apollo/client";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import { NavigationContainer } from "@react-navigation/native";
 import { StatusBar } from "expo-status-bar";
 
 import RootNavigator from "./navigation/RootNavigator";
-import SocialsIcons from "./components/SocialsIcons";
+// import SocialsIcons from "./components/SocialsIcons";
 import { getHost } from "./graphql/getHost";
+import { cache } from "./graphql/cache";
 
 const client = new ApolloClient({
   // with expo the following do not work
@@ -15,7 +16,7 @@ const client = new ApolloClient({
   // "http://localhost:4000/graphql"
   // "exp://192.168.1.65:19000/graphql"
   uri: getHost(),
-  cache: new InMemoryCache(),
+  cache,
   credentials: "include",
   defaultOptions: { watchQuery: { fetchPolicy: "cache-and-network" } },
 });
