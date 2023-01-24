782b150c82ca2cb11351fefc646a48c8bce039e2
CheoR <cheo.roman+github@gmail.com>
2023-01-24 09:11:19 -0600

    refactor: client to own file

--- App.tsx
+++ App.tsx
@@ -1,25 +1,11 @@
 import React from "react";
-import { ApolloClient, ApolloProvider } from "@apollo/client";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import { NavigationContainer } from "@react-navigation/native";
+import { ApolloProvider } from "@apollo/client";
 import { StatusBar } from "expo-status-bar";
 
 import RootNavigator from "./navigation/RootNavigator";
-// import SocialsIcons from "./components/SocialsIcons";
-import { getHost } from "./graphql/getHost";
-import { cache } from "./graphql/cache";
-
-const client = new ApolloClient({
-  // with expo the following do not work
-  // "http://localhost:4000/"
-  // "exp://192.168.1.65:19000/"
-  // "http://localhost:4000/graphql"
-  // "exp://192.168.1.65:19000/graphql"
-  uri: getHost(),
-  cache,
-  credentials: "include",
-  defaultOptions: { watchQuery: { fetchPolicy: "cache-and-network" } },
-});
+import { client } from "./graphql/client";
 
 export default function App() {
   return (
---
+++ graphql/client.ts
@@ -0,0 +1,16 @@
+import { ApolloClient } from "@apollo/client"
+
+import { getHost } from "./getHost"
+import { cache } from "./cache"
+
+export const client = new ApolloClient({
+  // with expo the following do not work
+  // "http://localhost:4000/"
+  // "exp://192.168.1.65:19000/"
+  // "http://localhost:4000/graphql"
+  // "exp://192.168.1.65:19000/graphql"
+  uri: getHost(),
+  cache,
+  credentials: "include",
+  defaultOptions: { watchQuery: { fetchPolicy: "cache-and-network" } },
+})
