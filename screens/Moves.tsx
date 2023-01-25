4abaa626823c0e2ec20a68b30e16502080c3ca16
CheoR <cheo.roman+github@gmail.com>
2023-01-25 15:15:47 -0600

    build: match moves gql request with backend

--- screens/Moves.tsx
+++ screens/Moves.tsx
@@ -1,11 +1,21 @@
 import React, { useState } from "react";
 import { Text, View, ScrollView, StyleSheet } from "react-native";
-// import { gql, useMutation } from "@apollo/client";
+import { gql, useMutation, useQuery } from "@apollo/client";
 
 import LoggedInLayout from "../layout/LoggedInLayout";
 import ListItem from "../components/ListItem";
 import Counter from "../components/Counter";
+import Loading from "../components/Loading";
 
+const GET_MOVES = gql`
+  query GetMoves {
+    getMovesByUserId {
+      _id
+      name
+      description
+    }
+  }
+`;
 // const CREATE_MOVE = gql`
 //   mutation CreateMove($input: CreateMoveInput!) {
 //     createMove(input: $input) {
@@ -32,17 +42,29 @@ import Counter from "../components/Counter";
 const createMove = () => console.log("oink");
 
 export default function MovesScreen() {
-  const [data, setData] = useState([]);
-
+  const { data, loading, error } = useQuery(GET_MOVES, {
+    onCompleted: (data) => {
+      // refresh data
+    },
+    onError: (error) => {
+      console.log(error.message);
+    },
+  });
+  if (loading) return <Loading text="Moves" />;
+  if (error) console.log("COULD NOT FETCH MOVES");
   return (
     <LoggedInLayout>
       <View style={styles.screen}>
         <Text>MovesScreen</Text>
         <View style={styles.scrollViewCntr}>
           <ScrollView contentContainerStyle={styles.list}>
-            {data.map((item) => (
-              <View key={item.id} style={styles.listItemCntr}>
-                <ListItem count={1} showValues={false} type={"move"} />)
+            {data.getMovesByUserId.map((item) => (
+              <View key={item._id} style={styles.listItemCntr}>
+                {/* <ListItem count={1} showValues={false} type={"move"} />) */}
+                <Text>Name</Text>
+                <Text>{item.name}</Text>
+                <Text>Description</Text>
+                <Text>{item.description}</Text>
               </View>
             ))}
           </ScrollView>
