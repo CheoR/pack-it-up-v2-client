ec6ab77cd2b6cd1d89de6f1ffb3caca03855c9ed
CheoR <cheo.roman+github@gmail.com>
2023-02-08 10:01:43 -0600

    todo: redirect to login screen after logout

--- components/Settings.tsx
+++ components/Settings.tsx
@@ -1,12 +1,15 @@
-import { View, Text } from "react-native";
 import React from "react";
+import { Button, Text, View } from "react-native";
+
 import LoggedInLayout from "../layout/LoggedInLayout";
+import { clearTokens } from "../auth/tokens";
 
 export default function SettingsScreen() {
   return (
     <LoggedInLayout>
       <View>
         <Text>Settings</Text>
+        <Button title="Logout" onPress={clearTokens} />
       </View>
     </LoggedInLayout>
   );
