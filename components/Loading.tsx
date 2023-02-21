779cacb5e44a20978a262582f8f626b3cf18292c
CheoR <cheo.roman+github@gmail.com>
2023-02-21 15:01:47 -0600

    chore: add string type

--- components/Loading.tsx
+++ components/Loading.tsx
@@ -4,30 +4,34 @@ import { StyleSheet, Text, View } from "react-native";
 import COLORS from "../constants/Colors";
 import { Icon } from "../constants/Icon";
 
-export default function Loading({ text }) {
+interface Loading {
+  text: string;
+}
+
+export default function Loading({ text }: Loading) {
   return (
     <View style={styles.loading}>
       <Icon type="closedPackage" size={208} />
-      <View style={styles.viewHeader}>
-        <Text style={styles.viewHeaderText}>Loading . . {text}</Text>
+      <View style={styles.header}>
+        <Text style={styles.text}>Loading . . {text}</Text>
       </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  loading: {
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  viewHeader: {
+  header: {
     display: "flex",
     alignItems: "center",
     width: "80%",
     paddingVertical: 8,
   },
-  viewHeaderText: {
+  loading: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  text: {
     color: COLORS.light.text,
   },
 });
