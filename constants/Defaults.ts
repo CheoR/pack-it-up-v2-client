7131002187207d362c65571882b87031add3df0d
CheoR <cheo.roman+github@gmail.com>
2023-02-22 07:18:17 -0600

    chore: create type defaults

---
+++ constants/Defaults.ts
@@ -0,0 +1,9 @@
+export const defaultItemCreate = {
+  // TODO: update to add a default box and/or
+  // allow for unbounded items
+  box_id: "63d58f28e9c4ff10994a0dca",
+  description: "",
+  name: "Item",
+  value: 0,
+  isFragile: false,
+};
