94a67d29ef47b06947aef782906dd6e25e33d2b2
CheoR <cheo.roman+github@gmail.com>
2023-02-28 15:48:02 -0600

    chore: add create and column one defaults

--- constants/Defaults.ts
+++ constants/Defaults.ts
@@ -1,3 +1,5 @@
+import { ColumnOne } from "../types/types";
+
 export const defaultItemCreate = {
   // TODO: update to add a default box and/or
   // allow for unbounded items
@@ -6,4 +8,28 @@ export const defaultItemCreate = {
   name: "Item",
   value: 0,
   isFragile: false,
+};
+
+export const defaultMoveCreate = {
+  name: "Move",
+  description: "",
+}
+
+export const defaultBoxCreate = {
+  // TODO: update to add a default box and/or
+  // allow for unbounded items
+  move_id: "63d2f72669850c57c9184e3c",
+  description: "",
+  name: "Box",
+}
+
+export const defaultItemColumnOne: ColumnOne = {
+  badge1: {
+    // need as const else get this error
+    // Type 'string' is not assignable to type 'PossibleIcons'.
+    size: 24 as const,
+    showCount: false,
+    showType: false,
+    type: "item" as const,
+  },
 };
