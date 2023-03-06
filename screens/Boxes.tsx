18c208b187be202026bfa8de4fb9790271733a55
CheoR <cheo.roman+github@gmail.com>
2023-03-06 15:43:58 -0600

    refactor: spread Box object properties instead of assigning all to obj

--- screens/Boxes.tsx
+++ screens/Boxes.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useMutation, useQuery } from "@apollo/client";
 
-import { Boxes, ColumnOne, ColumnThree, ColumnTwo } from "../types/types";
+import { Box, ColumnOne, ColumnThree, ColumnTwo } from "../types/types";
 import ScrollAndCounter from "../components/ScrollAndCounter";
 import { CREATE_BOX, GET_BOXES } from "../graphql/box";
 import Loading from "../components/Loading";
@@ -34,17 +34,29 @@ export default function BoxesScreen() {
       screen="Boxes"
       type="box"
     >
-      {data.getBoxesByUserId.map((box: Boxes) => {
+      {data.getBoxesByUserId.map((box: Box) => {
         let column1: ColumnOne = {
-          obj: { ...box, ...defaultListViewIconOptions },
-        };
-
-        let column2: ColumnTwo = {
           ...box,
-          ...isEditable,
+          ...defaultListViewIconOptions,
         };
 
-        return <Row key={box._id} column1={column1} column2={column2} />;
+        let column2: ColumnTwo<Box> = {
+          ...box,
+          ...defaultListViewIsEditable,
+        };
+        let column3: ColumnThree<Box> = {
+          ...box,
+          showIcon: true,
+        };
+
+        return (
+          <Row
+            key={box._id}
+            column1={column1}
+            column2={column2}
+            column3={column3}
+          />
+        );
       })}
     </ScrollAndCounter>
   );
