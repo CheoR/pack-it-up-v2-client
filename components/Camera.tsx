ab552bccb68dbf2b014b70ed01426e9b78ff5e6a
CheoR <cheo.roman+github@gmail.com>
2023-03-24 08:38:53 -0500

    build: user can take picture and upload image to cloudinary

--- components/Camera.tsx
+++ components/Camera.tsx
@@ -2,6 +2,8 @@ import React, { useEffect, useState, useRef } from "react";
 import { Image, Text, StyleSheet, TouchableOpacity, View } from "react-native";
 import { Camera as ExpoCamera, CameraType } from "expo-camera";
 
+import { manipulateAsync } from "expo-image-manipulator";
+
 import COLORS from "../constants/Colors";
 
 export default function App({ setFormFields }) {
@@ -18,23 +20,22 @@ export default function App({ setFormFields }) {
     (async () => {
       const cameraStatus = await ExpoCamera.requestCameraPermissionsAsync();
       setHasCameraPermission(cameraStatus.status === "granted");
-      // console.log(`available picture sizes`);
-      // let sizes = await cameraRef?.current?.getAvailablePictureSizesAsync(
-      //   "1:1"
-      // );
-      // console.log(sizes);
-      // setImageSize(sizes[0] || "320x320");
     })();
   }, []);
 
   async function takePicture() {
     console.log(`taking picture`);
-    console.log(cameraRef?.current?._cameraRef);
-    console.log(cameraRef?.current?.props.id);
-    console.log("------------");
     if (cameraRef.current !== null) {
-      console.log(`cameraRef.current: ${cameraRef.current._cameraRef}`);
-      console.log(`cameraRef.current: ${cameraRef.current.props.id}`);
+      console.log(cameraRef.current !== null);
+      console.log(`available picture sizes`);
+      let sizes = await cameraRef?.current?.getAvailablePictureSizesAsync(
+        "1:1"
+      );
+      let size = "100x100";
+      console.log(sizes);
+      if (sizes?.length) size = sizes[0];
+      setImageSize(size);
+
       try {
         //  Note: Make sure to wait for the onCameraReady callback before calling this method.
         console.log(`cameraRef.current try taking piocture`);
@@ -47,11 +48,39 @@ export default function App({ setFormFields }) {
         console.log(`base64: ${base64?.length}`);
         const backupImg =
           "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAoAAAAKCAMAAAC67D+PAAAAA1BMVEXr7MqLt5XVAAAAC0lEQVR4AWOgKwAAAG4AAfBdB/0AAAAASUVORK5CYII=";
+
+        console.log(`resigin immage`);
+        // https://docs.expo.dev/versions/latest/sdk/imagemanipulator/#action
+        // It is included if the base64 save option was truthy, and is a string containing
+        //  the JPEG/PNG (depending on format) data of the image in Base64. Prepend that
+        //   with 'data:image/xxx;base64,' to get a data URI, which you can use as the source
+        //    for an Image element for example (where xxx is jpeg or png).
+
+        const resizedPhoto = await manipulateAsync(
+          uri,
+          [{ resize: { width: 300 } }], // resize to width of 300 and preserve aspect ratio
+          { compress: 0.95, base64: true } // any higher than 0.95 causes error: base64 too long
+        );
+
+        console.log(`
+        before format uri lenght: ${uri.length}
+        before format base64 lenght: ${base64?.length}
+        after format uri length: ${resizedPhoto.uri.length}
+        after format uri length: ${resizedPhoto.base64?.length}
+        `);
+        let img = backupImg;
+        if (resizedPhoto.base64?.length) {
+          console.log(
+            ` has length resizedPhoto.base64?.length: ${resizedPhoto.base64?.length}`
+          );
+          img = "data:image/jpeg;base64,".concat(resizedPhoto.base64);
+        }
         setImage(
           //
           // uri
-          backupImg
+          // backupImg
           // base64 || backupImg
+          img
           // `https://upload.wikimedia.org/wikipedia/commons/a/ae/Olympic_flag.jpg`
         ); // base64);
         console.log(`data image dta`);
@@ -63,7 +92,7 @@ export default function App({ setFormFields }) {
 
   async function savePicture() {
     console.log(`i am image`);
-    console.log(image);
+    // console.log(image);
     if (image) {
       console.log(`safe function image`);
       // console.log(image);
@@ -98,6 +127,7 @@ export default function App({ setFormFields }) {
           type={type}
           ref={cameraRef}
           flashMode={flash}
+          // pictureSize={"100x100"}
           // pictureSize={imageSize}
           // ratio="4:3"
         >
